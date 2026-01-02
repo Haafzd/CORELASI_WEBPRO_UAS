40d170f39862cbef2971eca665eb0fb6091822b1
fadhlidzaki <fadhlidzaki120@gmail.com>
2026-01-02 18:24:54 +0700

    fix penugasan section on dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CorelasiTeacherOnlySeeder::class,
-            DummyDataSeeder::class,
             ScheduleSeeder::class,
+            DummyDataSeeder::class,
         ]);
 
         // User::factory(10)->create();
